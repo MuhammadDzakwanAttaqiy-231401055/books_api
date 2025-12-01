3fce1d7f5a7b5403c4af707636ac2d21101c38d3
MuhammadDzakwanAttaqiy-231401055 <151598799+MuhammadDzakwanAttaqiy-231401055@users.noreply.github.com>
2025-12-01 14:32:54 +0700

    Add files via upload

---
+++ db.js
@@ -0,0 +1,20 @@
+const { Pool } = require("pg");
+// ? .env = menyimpan data konfigurasi agar tidak langsung di tuliskan dalam kode
+// ? Dapat disebut juga variabel "global"
+const dotenv = require("dotenv");
+// ? Aktifkan dotenv
+dotenv.config();
+
+// ? Koneksi ke postgreSQL
+const pool = new Pool({
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  
+  ssl: {
+    rejectUnauthorized: false,
+  },
+  // port: process.env.PGPORT,
+});
+module.exports = pool;
